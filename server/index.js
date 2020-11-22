b5f741545aa8c08cc20cedfc1699bd7104752b39
ufuk <ufuk.uysl5@gmail.com>
2020-11-22 21:39:06 +0100

    update files

--- server/index.js
+++ server/index.js
@@ -2,22 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+//hide your key
+const dotenv = require("dotenv")
+dotenv.config()
+
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8081"
+  origin: "http://localhost:8081"
 };
 
 
 //mongoose connection
-mongoose.connect('mongodb+srv://UserName:<password>@cluster0-8vkls.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.CONNECTDB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => {
-  console.log(‘MongoDB Connected…’)
-})
-.catch(err => console.log(err))
+  .then(() => {
+    console.log("MongoDB Connected…")
+  })
+  .catch(err => console.log(err.message))
 
 
 
@@ -31,11 +35,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
